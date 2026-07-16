f101e6a1f434a5d3fffc781c52ca80b3e3e69273
Rolly93 <129896901+Rolly93@users.noreply.github.com>
2026-07-16 17:37:13 -0500

    Adding Notiifaction Mantiene at root level

--- Frontend/src/Router.tsx
+++ Frontend/src/Router.tsx
@@ -3,21 +3,20 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Layout } from './components/Layout';  
 import { DashBoard } from './pages/DashBoard';
 import { SftpConnection } from './pages/SftpConnection';
-// import { ProfilePage } from './pages/Profile.page'; // Example of another page
-
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />, // The persistent Shell
+    element: <Layout />,
     children: [
       {
-        path: '/', // This makes it the default page at '/'
+        path: '/', 
         element: <DashBoard />,
       },
       {
-          path: '/sftp_connection', // Matches '/profile' seamlessly inside the same shell
+          path: '/sftp_connection', 
          element: <SftpConnection/>
        },
+
     ],
   },
 ]);
