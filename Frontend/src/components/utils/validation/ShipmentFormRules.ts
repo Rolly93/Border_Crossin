20a31cd448f48f85ca4aa3362bcebbe0b2700eeb
Rolly93 <129896901+Rolly93@users.noreply.github.com>
2026-07-17 14:44:03 -0500

    Modify 'events' field to be able to evaluate
    aveluate the fields of events using a foreach so it can evaluate everysingle field of the datetime one by one

--- Frontend/src/components/utils/validation/ShipmentFormRules.ts
+++ Frontend/src/components/utils/validation/ShipmentFormRules.ts
@@ -1,7 +1,11 @@
 
 export const setupShipmentWatchers = (form: any) => {
-form.watch('cliente', () => form.validateField('costumer_tracking'));
+form.watch('cliente', () => form.validateField('cliente'));
 form.watch('costumer_tracking', () => form.validateField('costumer_tracking'));
 form.watch('trcking_Number' , ()=> form.validateField('trcking_Number'));
-form.watch('events', () =>  form.validateField('events'));
+form.watch('events',()=>{
+    form.values.events.forEach((_:any,index:number)=>{
+        form.validateField(`events.${index}.dateTime`);
+    })
+})
 };
