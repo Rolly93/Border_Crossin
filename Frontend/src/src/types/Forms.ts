64541aa5c943ea26651e50e141dfc6b7cbc53596
Rolly93 <129896901+Rolly93@users.noreply.github.com>
2026-07-30 11:11:30 -0500

    Delete Frontend/src/src directory
    it was duplicated by accident this directory 'src'

--- Frontend/src/src/types/Forms.ts
+++
@@ -1,7 +0,0 @@
-import { Shipment  } from '@/types/Shipment';
-
-export interface ShipmentFormProps {
-  initialData?: Partial<Shipment>;
-  onSubmit: (values: Omit<Shipment, 'id' | 'events'>) => void; 
-  onCancel: () => void;
-}
