5bd9cab7f7f1362b59230d53e717980d7045dcad
Rolly93 <129896901+Rolly93@users.noreply.github.com>
2026-07-31 15:20:35 -0500

    Delete src directory

--- src/service/NotificationService.ts
+++
@@ -1,9 +0,0 @@
-import { ApiNotificationStreamService } from "./notification/ApiNotificationStreamService"
-import { INotificationStreamService } from "./notification/INotificationStreamService"
-import { MockNotificationStreamService } from "./notification/MockNotificationStreamService"
-
-const ip = import.meta.env.VITE_API_URL||false
-console.log(ip)
-export const notificationsStreamService : INotificationStreamService = ip
-? new ApiNotificationStreamService(ip) 
-: new MockNotificationStreamService();
