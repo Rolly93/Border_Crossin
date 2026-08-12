e3545cb8809313caadf3c697c5b0f22438e9a34d
Rolando Rios <rolando_rios93@hotmail.com>
2026-08-11 23:09:49 -0500

    introducing the translation for the whole software with i18n

---
+++ Frontend/src/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import translationEs from '@/locales/es/translation.json';
+
+export const resources = {
+  es: { translation: translationEs },
+} as const;
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'es',
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
