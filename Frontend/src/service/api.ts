5deea24949ed8bf6347666c676cd9e4dec043ac4
Rolando Rios <rolando_rios93@hotmail.com>
2026-08-18 17:18:13 -0500

    fixing the importation of variable for the ip

--- Frontend/src/service/api.ts
+++ Frontend/src/service/api.ts
@@ -1,10 +1,10 @@
-import axios ,{ AxiosInstance } from 'axios'
+import axios, { AxiosInstance } from 'axios'
 
 
-export const createApiClient= (ip:string):AxiosInstance => {
+export const createApiClient = (ip: string): AxiosInstance => {
+    const baseURL = import.meta.env.VITE_API_URL || `http://${ip}:8000/api`
     return axios.create({
-        baseURL:`htt://${ip}:8000/api`,
-        headers:{ 'Content-Type': 'application/json',}
-    },)}
-
-
+        baseURL: baseURL,
+        headers: { 'Content-Type': 'application/json', }
+    },)
+}
