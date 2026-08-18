eeb7bc8d42682bc2cbaf627c8f507092cf111a14
Rolando Rios <rolando_rios93@hotmail.com>
2026-08-18 16:52:56 -0500

    fix: remove sqlite connect_args for postgresql engine

--- backend/databse.py
+++ backend/databse.py
@@ -7,10 +7,10 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./shipments.db")
 secret_key = os.getenv("SECRET_KEY")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+if DATABASE_URL and DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
