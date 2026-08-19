b9b978a34fd88a138a81cc2651f3511e043d953f
Rolando Rios <rolando_rios93@hotmail.com>
2026-08-19 17:01:41 -0500

    creating a hook useClient to load and update the data to server

--- Frontend/src/features/clients/hooks/useClients.ts
+++ Frontend/src/features/clients/hooks/useClients.ts
@@ -1,28 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ICliente } from "../types/Cliente";
 import { CLIENT } from "../mocks/ClientMock";
+import { clientService } from "../service/clientService";
 
 export function useClients() {
   const [clients, setClients] = useState<ICliente[]>(CLIENT);
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
 
-  const addClient = (newClientData: Partial<ICliente>) => {
-    const client: ICliente = {
-      id: newClientData.id || 0,
-      name: newClientData.name || "",
-      telefono: newClientData.telefono || "",
-      email: newClientData.email || [],
-      estatus: true,
-      sftService: false,
-      emailService: (newClientData.email?.length || 0) > 0,
-    };
-    setClients((prev) => [client, ...prev]);
-  };
+  useEffect(() => {
+    let isMounted = true
+    async function fetchClients() {
+      try {
+        setLoading(true)
 
-  const updateClient = (id: number, updatedData: Partial<ICliente>) => {
-    setClients((prev) =>
-      prev.map((c) => (c.id === id ? { ...c, ...updatedData } : c))
-    );
-  };
+        const data = await clientService.getAll();
+        if (isMounted) setClients(data)
+      } catch (error) {
+        if (isMounted) setError("Failed to load clients")
 
-  return { clients, addClient, updateClient };
+      } finally {
+        if (isMounted) setLoading(false)
+      }
+    }
+    fetchClients()
+    return () => { isMounted = false; }
+  }, [])
+
+  const addClient = async (newClientData: ICliente) => {
+    try {
+      const creatClient = await clientService.insert(newClientData)
+
+      setClients((prev) => [creatClient, ...prev])
+      return creatClient;
+    } catch (err) {
+      console.error("Error creating client:", err)
+      throw err;
+
+    }
+
+  }
+
+  const updateClient = async (id: number, updatedData: ICliente) => {
+    try {
+      const updated = await clientService.update(id, updatedData)
+      setClients((prev) =>
+        prev.map((c) => (c.id === id ? updated : c)));
+      return updated
+    } catch (error) {
+      console.error("Error updating client:", error)
+    }
+  }
+
+
+
+
+  return { clients, addClient, updateClient, loading, error };
 }
